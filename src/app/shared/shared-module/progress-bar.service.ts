b68d69534b51d313880732db277b6701ed2160e7
wei.zhang <wei.zhang12@ele.me>
2016-08-22 01:58:14 +0800

    improve loading animation

--- src/app/shared/shared-module/progress-bar.service.ts
+++ src/app/shared/shared-module/progress-bar.service.ts
@@ -4,11 +4,20 @@ import {
 
 @Injectable()
 export class ProgressBarService {
+private _selector: string = 'loading-holder';
+ private _element: HTMLElement;
   showflag: boolean = false;
+  constructor() {
+    this._element = document.getElementById(this._selector);
+    this._element.style.display = 'none';
+  }
+
   loadding() {
+    this._element.style.display = 'block';
     this.showflag = true;
   }
   componentLoading() {
+    this._element.style.display = 'none';
     this.showflag = false;
   }
 
